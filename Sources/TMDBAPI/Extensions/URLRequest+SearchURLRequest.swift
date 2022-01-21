05bcb385a7177316c7ed37dd28cfde1b8c3a614e
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-21 16:17:14 -0500

    fix path

--- Sources/TMDBAPI/Extensions/URLRequest+SearchURLRequest.swift
+++ Sources/TMDBAPI/Extensions/URLRequest+SearchURLRequest.swift
@@ -14,7 +14,7 @@ protocol SearchURLRequest {
 extension URLRequest: SearchURLRequest {
     static func multiSearch(language: String, region: String, page: Int, includeAdult: Bool, query: String) -> URLRequest {
         var components = urlComponent
-        components.path = "3/search/multi"
+        components.path = "/3/search/multi"
         components.queryItems?.append(URLQueryItem(name: "language", value: language))
         components.queryItems?.append(URLQueryItem(name: "page", value: "\(page)"))
         components.queryItems?.append(URLQueryItem(name: "include_adult", value: "\(includeAdult)"))
