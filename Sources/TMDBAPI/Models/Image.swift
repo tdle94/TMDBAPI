585ba234deb14c82d9246d98849cf49a3932ea60
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-26 23:11:17 -0500

    fix decoding image

--- Sources/TMDBAPI/Models/Image.swift
+++ Sources/TMDBAPI/Models/Image.swift
@@ -11,6 +11,7 @@ public struct TMDBImage: Decodable {
     public let id: Int
     public let backdrops: [Backdrop]
     public let posters: [Backdrop]
+    public let logos: [Backdrop]
 
     public var backdropLinks: [String] {
         return backdrops.compactMap { "https://image.tmdb.org/t/p/original/" + ($0.filePath ?? "") }
@@ -21,7 +22,7 @@ public struct TMDBImage: Decodable {
     }
     
     enum CodingKeys: String, CodingKey {
-        case id, backdrops, posters
+        case id, backdrops, posters, logos
     }
 
     public init() {
@@ -30,10 +31,11 @@ public struct TMDBImage: Decodable {
         posters = []
     }
     
-//    public init(from: Decoder) throws {
-//        let container = try from.container(keyedBy: CodingKeys.self)
-//        id = try container.decodeIfPresent(Int.self, forKey: .id) ?? -1
-//        backdrops = try container.decodeIfPresent([Backdrop].self, forKey: .backdrops) ?? []
-//        posters = try container.decodeIfPresent([Backdrop].self, forKey: .posters) ?? []
-//    }
+    public init(from: Decoder) throws {
+        let container = try from.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(Int.self, forKey: .id) ?? -1
+        backdrops = try container.decodeIfPresent([Backdrop].self, forKey: .backdrops) ?? []
+        posters = try container.decodeIfPresent([Backdrop].self, forKey: .posters) ?? []
+        logos = try container.decodeIfPresent([Backdrop].self, forKey: .logos) ?? []
+    }
 }
