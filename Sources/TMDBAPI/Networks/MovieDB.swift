387b32b44d741770201b3b51178cb6d44b90277d
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-26 23:30:43 -0500

    remove en-US

--- Sources/TMDBAPI/Networks/MovieDB.swift
+++ Sources/TMDBAPI/Networks/MovieDB.swift
@@ -14,17 +14,17 @@ public struct MovieDB {
 
     public init() { }
 
-    public func detail(id: Int, language: String = "en-US") async throws -> Result<MovieDetail, TMDBMessage> {
+    public func detail(id: Int, language: String = "") async throws -> Result<MovieDetail, TMDBMessage> {
         let urlRequest = Movie.detail(id: id, language: language).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieDetail.self)
     }
 
-    public func credits(id: Int, language: String = "en-US") async throws -> Result<Credit, TMDBMessage> {
+    public func credits(id: Int, language: String = "") async throws -> Result<Credit, TMDBMessage> {
         let urlRequest = Movie.credit(id: id, language: language).urlRequest
         return try await network.performURLRequest(urlRequest, type: Credit.self)
     }
 
-    public func images(id: Int, language: String = "en-US") async throws -> Result<TMDBImage, TMDBMessage> {
+    public func images(id: Int, language: String = "") async throws -> Result<TMDBImage, TMDBMessage> {
         let urlRequest = Movie.images(id: id, language: language).urlRequest
         return try await network.performURLRequest(urlRequest, type: TMDBImage.self)
     }
@@ -34,17 +34,17 @@ public struct MovieDB {
         return try await network.performURLRequest(urlRequest, type: Keyword.self)
     }
     
-    public func recommendations(id: Int, language: String = "en-US", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
+    public func recommendations(id: Int, language: String = "", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
         let urlRequest = Movie.recommendations(id: id, language: language, page: page).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieType.self)
     }
     
-    public func similars(id: Int, language: String = "en-US", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
+    public func similars(id: Int, language: String = "", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
         let urlRequest = Movie.similars(id: id, language: language, page: page).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieType.self)
     }
     
-    public func videos(id: Int, language: String = "en-US") async throws -> Result<Video, TMDBMessage> {
+    public func videos(id: Int, language: String = "") async throws -> Result<Video, TMDBMessage> {
         let urlRequest = Movie.videos(id: id, language: language).urlRequest
         return try await network.performURLRequest(urlRequest, type: Video.self)
     }
@@ -54,27 +54,27 @@ public struct MovieDB {
         return try await network.performURLRequest(urlRequest, type: WatchProviders.self)
     }
 
-    public func nowPlaying(language: String = "en-US", region: String = "US", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
+    public func nowPlaying(language: String = "", region: String = "", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
         let urlRequest = Movie.nowPlaying(language: language, page: page, region: region).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieType.self)
     }
     
-    public func popular(language: String = "en-US", region: String = "US", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
+    public func popular(language: String = "", region: String = "", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
         let urlRequest = Movie.popular(language: language, page: page, region: region).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieType.self)
     }
     
-    public func topRated(language: String = "en-US", region: String = "US", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
+    public func topRated(language: String = "", region: String = "", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
         let urlRequest = Movie.topRated(language: language, page: page, region: region).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieType.self)
     }
     
-    public func upcoming(language: String = "en-US", region: String = "US", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
+    public func upcoming(language: String = "", region: String = "", page: Int = 1) async throws -> Result<MovieType, TMDBMessage> {
         let urlRequest = Movie.upcoming(language: language, page: page, region: region).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieType.self)
     }
     
-    public func latest(language: String = "en-US") async throws -> Result<MovieDetail, TMDBMessage> {
+    public func latest(language: String = "") async throws -> Result<MovieDetail, TMDBMessage> {
         let urlRequest = Movie.latest(language: language).urlRequest
         return try await network.performURLRequest(urlRequest, type: MovieDetail.self)
     }
