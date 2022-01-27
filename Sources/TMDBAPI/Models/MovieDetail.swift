bc7ee2ab8573f48bff79a7289c354b2f073c1bef
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-26 22:41:45 -0500

    assign value to display object

--- Sources/TMDBAPI/Models/MovieDetail.swift
+++ Sources/TMDBAPI/Models/MovieDetail.swift
@@ -51,6 +51,8 @@ public struct MovieDetail: Decodable {
         displayObj.images = images
         displayObj.totalSimilars = similar.totalResults
         displayObj.totalRecommends = similar.totalResults
+        displayObj.displayImageLinks = images.backdropLinks.getPrefix(5)
+        displayObj.displaySameObjects = recommendations.displayObjects
         return displayObj
     }
 
