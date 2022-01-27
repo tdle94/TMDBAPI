2b212f16b828e9754e33249a78f7ea10f7f0049e
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-26 22:05:57 -0500

    fix decoding

--- Sources/TMDBAPI/Models/Image.swift
+++ Sources/TMDBAPI/Models/Image.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct TMDBImage: Decodable {
-    public let id: Int?
+    public let id: Int
     public let backdrops: [Backdrop]
     public let posters: [Backdrop]
 
@@ -18,5 +18,16 @@ public struct TMDBImage: Decodable {
 
     public var posterLinks: [String] {
         return posters.compactMap { "https://image.tmdb.org/t/p/original/" + ($0.filePath ?? "") }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, backdrops, posters
+    }
+    
+    public init(from: Decoder) throws {
+        let container = try from.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(Int.self, forKey: .id) ?? -1
+        backdrops = try container.decodeIfPresent([Backdrop].self, forKey: .backdrops) ?? []
+        posters = try container.decodeIfPresent([Backdrop].self, forKey: .posters) ?? []
     }
 }
